76897a8ebf5def2069f7e952878a94bf176817a1
刘子聪 <zicong.liu.nju@gmail.com>
2016-03-29 14:48:25 +0800

    Fix format errors

--- answers/Pro231_2.java
+++ answers/Pro231_2.java
@@ -1,15 +1,21 @@
 public class Solution {
     public boolean isPowerOfTwo(int n) {
-    	if (n <= 0){
+    	if (n <= 0)
     		return false;
-    	}
         int cur = (int)n - 1;
         while (cur != 0){
-        	if ((cur & 1) != 1){
+        	if ((cur & 1) != 1)
         		return false;
-        	}
         	cur = (cur >> 1);
         }
         return true;
+    }
+}
+// OR JUST!!!
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return (n != 0
+         && n != Integer.MIN_VALUE
+          && (n & (n - 1)) == 0);
     }
 }
--- answers/Pro31.java
+++ answers/Pro31.java
@@ -2,18 +2,16 @@ public class Solution {
     public void nextPermutation(int[] nums) {
         int preIdx = -1 , postIdx = -1;
         for (int i = 0 ; i < nums.length - 1 ; i ++){
-            if (nums[i] < nums[i + 1]){
+            if (nums[i] < nums[i + 1])
                 preIdx = i;
-            }
         }        
         if (preIdx < 0){
             reverse(nums , 0 , nums.length - 1);
             return;
         }
         for (int i = preIdx + 1; i < nums.length ; i ++){
-            if (nums[preIdx] < nums[i]){
+            if (nums[preIdx] < nums[i])
                 postIdx = i;
-            }
         }
         int tmp = nums[preIdx];
         nums[preIdx] = nums[postIdx];
